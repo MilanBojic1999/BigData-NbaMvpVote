495be24208c0ac0e3f29119dc070f892e9fd45b3
milan boi <mbojic12@gmail.com>
2022-01-01 23:01:21 +0100

    Award voting

--- awards_voting.py
+++ awards_voting.py
@@ -26,9 +26,20 @@ def get_mvp_voting(year):
     mvp_table = mvp_table.findAll('table',{'id':'mvp'})[0]
 
     rows = get_table(mvp_table)
-    print(type(rows))
-    for n,s in rows:
-        print(n,s)
+    
+    result = []
+    for name,stats in rows:
+        tmp = [name,year]+stats
+        result.append(tmp)
+
+    for mini_r in result:
+        for ind in range(10,20):
+            if str(mini_r[ind]).startswith('.'):
+                mini_r[ind] = '0'+mini_r[ind]
+            elif mini_r[ind]=='':
+                mini_r[ind] = '0.0'
+        
+    return result
 
 def get_dpoy_voting(year):
     url = f"https://www.basketball-reference.com/awards/awards_{year}.html"
@@ -37,31 +48,49 @@ def get_dpoy_voting(year):
 
     soup = BeautifulSoup(html, features="html.parser")
 
+    for comment in soup(text=lambda it:isinstance(it,Comment)):
+        if 'id="div_dpoy"' in comment.string:
+            tag = BeautifulSoup(comment,'html.parser')
+            comment.replace_with(tag)
+            break
+
     dpoy_table = soup.findAll('div',{'id':"all_dpoy"})
     
     dpoy_table = dpoy_table[0]
 
-    for element in dpoy_table(text=lambda it:isinstance(it,Comment)):
-        element.extract()
-        
-
-    print(dpoy_table.prettify())
-    ddd = dpoy_table.findAll('div')
-    for d in ddd:
-        if d.has_attr('id'):
-            print(d['id'],d['class'])
-        else:
-            print(d['class'],'__',d.getText())
-
-    #dpoy_table = dpoy_table.findAll('div',{'id':"div_dpoy"})[0]
 
     dpoy_table = soup.findAll('table',{'id':'dpoy'})[0]
 
     rows = get_table(dpoy_table)
-    print(type(rows))
-    for n,s in rows:
-        print(n,s)
 
-year = 2021
+    result = []
+    for name,stats in rows:
+        tmp = [name,year]+stats
+        result.append(tmp)
+    
+    for mini_r in result:
+        for ind in range(10,20):
+            if str(mini_r[ind]).startswith('.'):
+                mini_r[ind] = '0'+mini_r[ind]
+            elif mini_r[ind]=='':
+                mini_r[ind] = '0.0'
+        
+    return result
 
-get_dpoy_voting(2021)
+def get_votings(year):
+    headers = ['Player','Year','Age','Team','FirstPlace','PtsWon','PtsMax','%','GP','MP','PTS','TRP','AST','STL','BLK','FG%','3P%','FT%','WS','WS/48']
+
+    mvp = get_mvp_voting(year)
+    dpoy = get_dpoy_voting(year)
+
+    stand = pd.DataFrame(mvp,columns=headers)
+    stand.to_csv(f"mvp_voting/mvp_{year}.csv",index=False)
+    
+    stand = pd.DataFrame(dpoy,columns=headers)
+    stand.to_csv(f"dpoy_voting/dpoy_{year}.csv",index=False)
+
+
+
+for y in range(2016,2022):
+    get_votings(y)
+    print('Done year',y)
